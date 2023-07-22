dd83d59d05091803f8ae4bf39990aedba2c2a41e
Mars Notahacker <marsXrobertson@gmail.com>
2023-07-22 21:11:59 +0200

    Page for display data

---
+++ src/pages/display.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewPage: React.FC = () => {
+  return (
+    <div>
+      <h1>New Page</h1>
+      <p>This is a new page in Next.js with React and TypeScript. Greg can handle the display data maybe?</p>
+    </div>
+  );
+};
+
+export default NewPage;
