bbda913cb9ec729f5c1d159016c696fe891a1b11
Mars Notahacker <marsXrobertson@gmail.com>
2023-07-23 04:15:44 +0200

    Display fixes

--- src/pages/display.tsx
+++ src/pages/display.tsx
@@ -80,48 +80,119 @@ const Display: React.FC = () => {
 			<p className=" text-3xl font-bold text-center mt-4 mb-12">
 				Submitted questionaires info
 			</p>
-			{data[0]?.votes.map((_vote, index) => (
-				<div className="my-3 card outline-black/10 outline shadow-md">
-					<div className="card-body">
-						<p className="mb-1 text-black/60">
-							Responses for question {index + 1}<br/>
-                            { questions[index].title }
-						</p>
-						<div className="max-w-md ml-auto">
-							<Bar
-								options={{
-									indexAxis: "y" as const,
-									elements: {
-										bar: {
-											borderWidth: 2,
+			{data[0]?.votes.map((_vote, index) => {
+				if (typeof _vote === "boolean" || typeof _vote === "string")
+					return (
+						<div className="my-3 card outline-black/10 outline shadow-md">
+							<div className="card-body">
+								<p className="mb-1 text-black/60">
+									Responses for question {index + 1}<br/>
+									<span className="text-2xl">{ questions[index].title }</span>
+								</p>
+								<div className="max-w-md ml-auto">
+									<Pie
+										options={{
+											indexAxis: "y" as const,
+											elements: {
+												bar: {
+													borderWidth: 2,
+												},
+											},
+											responsive: true,
+										}}
+										data={{
+											labels: ["Yes", "No", "Undecided"],
+											datasets: [
+												{
+													data: [
+														//@ts-ignore can vote can be number,bool, string
+														data.filter((item) => item.votes[index]! === true)
+															.length,
+														//@ts-ignore
+														data.filter((item) => item.votes[index]! === false)
+															.length,
+
+														data.filter(
+															//@ts-ignore
+															(item) => item.votes[index]! === "undecided"
+														).length,
+													],
+													label: "Vote",
+													borderColor: [
+														"rgba(54, 162, 235, 1)",
+														"rgba(255, 99, 132, 1)",
+														"rgba(153, 102, 255, 1)",
+													],
+													backgroundColor: [
+														"rgba(54, 162, 235, 0.2)",
+														"rgba(255, 99, 132, 0.2)",
+														"rgba(153, 102, 255, 0.2)",
+													],
+												},
+											],
+										}}
+									/>
+								</div>
+							</div>
+						</div>
+					);
+				return (
+					<div className="my-3 card outline-black/10 outline shadow-md">
+						<div className="card-body">
+							<p className="mb-1 text-black/60">
+								Responses for question {index + 1}<br/>
+								<span className="text-2xl">{ questions[index].title }</span>
+							</p>
+							<div className="max-w-md ml-auto">
+								<Bar
+									options={{
+										indexAxis: "y" as const,
+										elements: {
+											bar: {
+												borderWidth: 2,
+											},
 										},
-									},
-									responsive: true,
-								}}
-								data={{
-									labels: Array.from(
-										{ length: data.length },
-										(_, i) => `User ${i}`
-									),
-									datasets: [
-										{
-											data: Array.from(
-												{ length: data.length! },
-												(_, i) => data[i].votes[index]
-											),
-											label: "Vote",
-											borderColor: "rgb(52, 79, 203)",
-											backgroundColor: "rgba(99, 151, 255, 0.5)",
-										},
-									],
-								}}
-							/>
+										responsive: true,
+									}}
+									data={{
+										labels: Array.from(
+											{ length: data.length },
+											(_, i) => `User ${i}`
+										),
+										datasets: [
+											{
+												data: Array.from(
+													{ length: data.length! },
+													(_, i) => data[i].votes[index]
+												),
+												label: "Vote",
+												backgroundColor: [
+													"rgba(255, 99, 132, 0.2)",
+													"rgba(54, 162, 235, 0.2)",
+													"rgba(255, 206, 86, 0.2)",
+													"rgba(75, 192, 192, 0.2)",
+													"rgba(153, 102, 255, 0.2)",
+													"rgba(255, 159, 64, 0.2)",
+												],
+												borderColor: [
+													"rgba(255, 99, 132, 1)",
+													"rgba(54, 162, 235, 1)",
+													"rgba(255, 206, 86, 1)",
+													"rgba(75, 192, 192, 1)",
+													"rgba(153, 102, 255, 1)",
+													"rgba(255, 159, 64, 1)",
+												],
+											},
+										],
+									}}
+								/>
+							</div>
 						</div>
 					</div>
-				</div>
-			))}
+				);
+			})}
 			{/* Used for Mockup Reasons */}
-			{/* <div className="my-3 card outline-black/10 outline shadow-md">
+			<div className="my-3 card outline-black/10 outline shadow-md">
 				<div className="card-body">
 					<p className="mb-1 text-black/60">Responses for question #0</p>
 					<div className="w-full ">
@@ -142,26 +213,75 @@ const Display: React.FC = () => {
 									{
 										data: [-2, -1, 4],
 										label: "Vote",
-										borderColor: "rgb(52, 79, 203)",
-										backgroundColor: "rgba(99, 151, 255, 0.5)",
+										backgroundColor: [
+											"rgba(255, 99, 132, 0.2)",
+											"rgba(54, 162, 235, 0.2)",
+											"rgba(255, 206, 86, 0.2)",
+											"rgba(75, 192, 192, 0.2)",
+											"rgba(153, 102, 255, 0.2)",
+											"rgba(255, 159, 64, 0.2)",
+										],
+										borderColor: [
+											"rgba(255, 99, 132, 1)",
+											"rgba(54, 162, 235, 1)",
+											"rgba(255, 206, 86, 1)",
+											"rgba(75, 192, 192, 1)",
+											"rgba(153, 102, 255, 1)",
+											"rgba(255, 159, 64, 1)",
+										],
 									},
 								],
 							}}
 						/>
 					</div>
 				</div>
-			</div> */}
-
-			{/* <ul>
+			</div>
+			<div className="my-3 card outline-black/10 outline shadow-md">
+				<div className="card-body">
+					<p className="mb-1 text-black/60">Responses for question #0</p>
+					<div className="w-full ">
+						<Pie
+							className="lg:w-3/4 ml-auto"
+							options={{
+								indexAxis: "y" as const,
+								elements: {
+									bar: {
+										borderWidth: 2,
+									},
+								},
+								responsive: true,
+							}}
+							data={{
+								labels: ["Yes", "No", "Undecided"],
+								datasets: [
+									{
+										data: [23, 54, 12],
+										label: "Vote",
+										borderColor: [
+											"rgba(54, 162, 235, 1)",
+											"rgba(255, 99, 132, 1)",
+											"rgba(153, 102, 255, 1)",
+										],
+										backgroundColor: [
+											"rgba(54, 162, 235, 0.2)",
+											"rgba(255, 99, 132, 0.2)",
+											"rgba(153, 102, 255, 0.2)",
+										],
+									},
+								],
+							}}
+						/>
+					</div>
+				</div>
+			</div>
+			<ul>
 				{data.map((item, index) => (
 					<li key={index}>
 						<strong>Votes:</strong> {item.votes.join(", ")},{" "}
 						<strong>WorldID:</strong> {item.worldID.toString()}
 					</li>
 				))}
-			</ul> */}
-
-            
+			</ul>
 		</div>
 	);
 };
